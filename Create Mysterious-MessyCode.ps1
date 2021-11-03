9661da81ec48acca48f948e4b4bef8e6dcc9b9bf
shayagoldbergitc <93670479+shayagoldbergitc@users.noreply.github.com>
2021-11-03 13:03:31 -0400

    Add files via upload

---
+++ Create Mysterious-MessyCode.ps1
@@ -0,0 +1,4 @@
+﻿ $Appname = Read-Host "Search for Installed App"
+ $Uninstallers = Get-ChildItem HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*,HKLM:\Software\WoW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*;
+ ($Uninstallers|% {$a=$_;
+Get-ItemProperty -name UninstallString -Path "$($a.name.replace('HKEY_LOCAL_MACHINE','HKLM:'))" -ErrorAction SilentlyContinue|? {(Get-ItemProperty -name DisplayName -ErrorAction SilentlyContinue -Path $a.name.replace('HKEY_LOCAL_MACHINE','HKLM:')).DisplayName -like "*$Appname*"}}).UninstallString -replace '"',"" |Invoke-Item
